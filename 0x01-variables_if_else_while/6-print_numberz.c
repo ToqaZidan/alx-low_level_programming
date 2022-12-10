e34ff9efd5688a37715265c66927130569941968
ToqaZidan <toqazidan@hotmail.com>
2022-12-09 17:03:41 -0800

    tasks

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - print the alphapet lowercase
+ * main - print numbers
  *
  * Return: Always 0
  */
@@ -11,9 +11,9 @@ int main(void)
 	int num;
 
 	for (num = 0; num < 10; num++)
-		printf("%d", num);
+		putchar((num % 10) + '0');
 
-	printf("\n");
+	putchar('\n');
 
 	return (0);
 }
