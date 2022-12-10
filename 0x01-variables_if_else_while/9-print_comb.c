3ed34c9d240e843f173967de5a70202f8b201278
ToqaZidan <toqazidan@hotmail.com>
2022-12-09 17:22:21 -0800

    tasks

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
+
+		putcahr(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
