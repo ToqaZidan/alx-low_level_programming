725554e661db9a4c8e3217cb904f2239aa6e9d09
ToqaZidan <toqazidan@hotmail.com>
2022-12-13 04:02:07 -0800

    task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <main.h>
+
+/**
+ * main - a programe that prints _putchar, followed by a new line.
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	_putchar("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Tasks about function and nested loops, i'm not allaowed to use STD LIB .. i have to use only _putchar.
