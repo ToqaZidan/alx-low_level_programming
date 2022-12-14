5b7e04bd7ac040aafd92b8780f145ec946a90aee
ToqaZidan <toqazidan@hotmail.com>
2022-12-13 17:28:32 -0800

    tasks

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - add two intgers
+ *
+ * @a: an integer to be added
+ * @b: an integer to be added
+ *
+ * Return: Result of addation
+ */
+int add(int a, int b)
+{
+	return(a + b);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * times_table - print the nine times table, starts woth zero
+ */
+void times_table(void)
+{
+	int num, multi, prd;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar('0');
+
+		for (multi = 1; multi <= 9; multi++);
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prd = num * multi;
+
+			if (prd <= 9);
+				_putchar(' ');
+			else
+				_putchar((prd / 10) = '0');
+
+			_putcahr((prd % 10) + '0');
+		}
+	_putchar('\n');
+
+	}
+}	
