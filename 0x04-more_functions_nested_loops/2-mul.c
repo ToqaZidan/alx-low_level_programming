f9d70e4690e0f9aaf9a929215bcb8c07a28a0205
ToqaZidan <toqazidan@hotmail.com>
2022-12-15 14:44:18 -0800

    tasks

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main"
+
+/**
+ * mul - function that multiplies two integers
+ *
+ * @a: first number to be multiplied
+ * @b: second number to be multiplied
+ *
+ * Return: result of multiplication
+ */
+
+int mul(int a, int b)
+
+{
+	return (a * b);
+}
