da681d20d59d5626bcfe4ee22530e6608f19aa6f
ToqaZidan <toqazidan@hotmail.com>
2022-12-15 15:38:15 -0800

    tasks

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -17,5 +17,5 @@ void print_most_numbers(void)
 			_putchar((n % 10) + '0');
 	}
 
-	putchar('\n')
+	_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -19,7 +19,7 @@ void more_numbers(void)
 		if
 			(n > 9)
 				_putchar((n / 10) + '0');
-		putchar((n % 10) + '0');
+		_putchar((n % 10) + '0');
 	}
 	_putchar('\n');
 }
