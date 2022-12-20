a177e617ff8d4b25ea3375f82d5e77e626f8a92b
ToqaZidan <toqazidan@hotmail.com>
2022-12-19 18:05:53 -0800

    tasks

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,15 +9,14 @@
  *   Return: A pointer to the destination string @dest.
  */
 
-char *_strcpy(char *dest, const char *src)
+char *_strcpy(char *dest, char *src)
 {
-	int index = 0;
+	char *p = dest;
 
-	while (src[index])
-	{
-		dest[index] = src[index];
-		index++;
-	}
+	while (*src)
+		*p++ = *src++;
+	*p = '\0';
 
-	return (dest);
+	return dest;
 }
+
