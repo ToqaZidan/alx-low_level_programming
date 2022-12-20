c37a628d96aa3db554d79e0b1cf216a5cab0c1cb
ToqaZidan <toqazidan@hotmail.com>
2022-12-19 16:52:26 -0800

    tasks

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,14 +7,13 @@
 
 void rev_string(char *s)
 {
-	int len = 0,
-	    index = 0,
-	    char tmp;
+	int len = 0, index = 0;
+		char tmp;
 
 	while (s[index++])
 		len++;
 
-	for (index = len -1; index >= len / 2; index__)
+	for (index = len -1; index >= len / 2; index--)
 	{
 		tmp = s[index];
 		s[index] = s[len - index - 1];
