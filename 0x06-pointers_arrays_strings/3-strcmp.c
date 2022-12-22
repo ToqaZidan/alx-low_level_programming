4c340ec1cce0b6710cfba326a5ebd0c04683f2be
ToqaZidan <toqazidan@hotmail.com>
2022-12-22 12:53:03 -0800

    task

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,9 +6,9 @@
  * @s1: First string
  * @s2: Second string
  *
- * Return: Value less than 0 if string is less than the other
- Value greater than 0 if string is greater then the other
- 0 if strings are equal
+ * Return: Value less than 0 if string is less than the other;
+ * Value greater than 0 if string is greater then the other;
+ * 0 if strings are equal
  */
 
 int _strcmp(char *s1, char *s2)
