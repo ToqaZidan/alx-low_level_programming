dfd9d996d9ae643ff276f84a08ef7c30276455d5
ToqaZidan <toqazidan@hotmail.com>
2023-01-11 11:28:15 -0800

    Task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - function that print the name of the file
+ * @argc: count argument
+ * @argv: argument
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(int argc, char **argv)
+{
+	(void) argc;
+
+	printf("%s \n", argv[0]);
+
+	return (0);
+}
