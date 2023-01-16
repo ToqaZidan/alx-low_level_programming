acc16180b0b2048566d0a9952128685974659fdc
ToqaZidan <toqazidan@hotmail.com>
2023-01-16 10:56:55 -0800

    Tasks

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include "main.h"
+
 /**
  * main - Function that prints the file name
  * Description: programe that prints the name of a file
  *
- * Return: 0 (success) 
+ * Return: 0 (success)
  */
 
 int main(void)
