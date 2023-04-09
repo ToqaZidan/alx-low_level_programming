74533168eb9a3fbdb42d05ddfa2cb933e9b71f5b
ToqaZidan <toqazidan@hotmail.com>
2023-04-09 16:35:08 -0700

    task2

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,15 @@
+/**
+ * key_index - A function that gets the index of a key in hash table
+ *
+ * @key: The key to get the index of.
+ * @size: The size of the array of the hash table.
+ *
+ * Description: shoul uses the djb2 algorithm.
+ *
+ * Return: The index of the key.
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
