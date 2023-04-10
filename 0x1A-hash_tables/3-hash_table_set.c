2b14951fb8e7318b12c41a5507990e85b04debc3
ToqaZidan <toqazidan@hotmail.com>
2023-04-10 13:27:36 -0700

    task0

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,49 +11,43 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    unsigned long int index;
-    hash_node_t *new_node, *current;
+	hash_node_t *new_node;
+	char *cpvalue;
+	unsigned long int index;
 
-    if (ht == NULL || key == NULL || *key == '\0')
-        return (0);
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
 
-    index = key_index((const unsigned char *)key, ht->size);
-    current = ht->array[index];
+	cpvalue = strdup(value);
+	if (cpvalue == NULL)
+		return (0);
 
-    while (current != NULL)
-    {
-        if (strcmp(current->key, key) == 0)
-        {
-            free(current->value);
-            current->value = strdup(value);
-            if (current->value == NULL)
-                return (0);
-            return (1);
-        }
-        current = current->next;
-    }
+	index = key_index((const unsigned char *)key, ht->size);
+	for (ht->array[index]; index++)
+	{
+		if (strcmp(ht->array[index]->key, key) == 0)
+		{
+			free(ht->array[index]->value);
+			ht->array[index]->value = cpvalue;
+			return (1);
+		}
+	}
 
-    new_node = malloc(sizeof(hash_node_t));
-    if (new_node == NULL)
-        return (0);
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+	{
+		free(cpvalue);
+		return (0);
+	}
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (0);
+	}
+	new_node->value = cpvalue;
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
 
-    new_node->key = strdup(key);
-    if (new_node->key == NULL)
-    {
-        free(new_node);
-        return (0);
-    }
-
-    new_node->value = strdup(value);
-    if (new_node->value == NULL)
-    {
-        free(new_node->key);
-        free(new_node);
-        return (0);
-    }
-
-    new_node->next = ht->array[index];
-    ht->array[index] = new_node;
-
-    return (1);
+	return (1);
 }
