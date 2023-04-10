d11da57796841c55a75f79a8bb8eb9040a6e534d
ToqaZidan <toqazidan@hotmail.com>
2023-04-10 13:11:10 -0700

    task3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
     unsigned long int index;
     hash_node_t *new_node, *current;
 
-    if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+    if (ht == NULL || key == NULL || *key == '\0')
         return (0);
 
     index = key_index((const unsigned char *)key, ht->size);
