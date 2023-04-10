5a93c4ae98a2986800d290e8c8ef3a48daeb3f3e
ToqaZidan <toqazidan@hotmail.com>
2023-04-10 13:36:53 -0700

    task

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,17 +13,18 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht = malloc(sizeof(hash_table_t));
 
-	if (ht == NULL)
+	if (ht == NULL){
 
 		return (NULL);
+	}
 
 	ht->size = size;
 	ht->array = calloc(size, sizeof(hash_node_t *));
 
-	if (ht->array == NULL)
-	free(ht);
-	return (NULL);
-
+	if (ht->array == NULL){
+		free(ht);
+		return (NULL);
+	}
 
 	return (ht);
 }
