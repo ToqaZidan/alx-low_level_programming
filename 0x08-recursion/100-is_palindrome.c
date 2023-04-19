41f0ef25260e5eb6a22dc6dfd088e2b6d0b6f657
ToqaZidan <toqazidan@hotmail.com>
2023-04-19 08:10:33 -0700

    delfordefr

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -8,7 +8,7 @@
  */
 int palindromeChecker(char *str, int len, int i)
 {
-	if (i < len && str[i] == str[len])
+	if ((i < len) && (str[i] == str[len]))
 			return (palindromeChecker(str, len - 1, i + 1));
 	if (str[i] != str[len])
 		return (0);
