d59e182d545356598f4a4e58ecfb84986522000c
Toqa Zidan <toqazidan@hotmail.com>
2023-08-03 16:52:39 -0700

    ..

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ A module to calculate the perimeter of the island described in grid"""
 
+
 def island_perimeter(grid):
     """
     Calculate the perimeter of the island described in the grid.
